42ad01ab8afbea836fd45e35ae984cfb92bd4dd2
Amrita Bhattarai <amritachi58@gmai.com>
2023-08-18 09:13:49 +0545

    Update:Convert JavaScript to TypeScript

---
+++ mergeSort.ts
@@ -0,0 +1,48 @@
+
+const merge = (unsortedArr:number[],low:number,mid:number,high:number)=>{
+    let mergeArr =[]
+    let i=low;
+    let j=mid+1;
+    let k=0;
+    while(i<= mid && j<= high){
+        if(unsortedArr[i]<unsortedArr[j]){
+            mergeArr[k] = unsortedArr[i]
+            i+=1;
+            k+=1;
+            console.log(i,k)
+        }
+        else{
+            mergeArr[k] = unsortedArr[j]
+            j+=1;
+            k+=1;
+            console.log(j,k)
+    }
+}
+    while(i<=mid){
+        mergeArr[k] = unsortedArr[i]
+        i+=1
+        k+=1
+    }
+    while(j<=high){
+        mergeArr[k] = unsortedArr[j]
+        j+=1
+        k+=1
+    }
+    for (let i = low; i <= high; i++) {
+        unsortedArr[i] = mergeArr[i - low];
+      }
+}
+
+const mergeSort = (unsortedArr:number[],low:number,high:number)=>{
+    if(low<high){
+        let mid=Math.floor((low+high)/2)
+        mergeSort(unsortedArr,low,mid)
+        mergeSort(unsortedArr,mid+1,high)
+        merge(unsortedArr,low,mid,high)
+    }
+}
+
+let a = [7,6,5,1,4,8,10]
+
+mergeSort(a, 0 ,a.length-1)
+console.log(a)
